baf162bc29aacb0f8962e55ff4605fb63fbe98c1
Evan <147341529+EvanJosephL@users.noreply.github.com>
2024-08-17 21:42:06 -0500

    Add files via upload

--- OldSchoolRunescape Examples/Scorpion_Grinder/scorpion_grinder.py
+++ OldSchoolRunescape Examples/Scorpion_Grinder/scorpion_grinder.py
@@ -1,17 +1,19 @@
 import pybot as pb
 import keyboard as kb
 import cv2 as cv
-import pyautogui as pya
 
 def main():
-    acc_list = []
+    instance_list = []
+    HWND = []
     win_ctr = 0
-    for win in pya.getAllWindows():
-        if "RuneLite - " in win.title:
-            acc_list.append(win.title)
+    n_ctr = 0
+    windows = pb.getWindows()
+    for win in windows:
+        if "RuneLite - " in win:
+            instance_list.append(win)
             win_ctr += 1
 
-    pb.getHwnd(acc_list)
+    HWND = pb.getHwnd(instance_list)
     window_num = 0
 
     scorpion_hsv = [0, 0, 0, 0, 255, 65, 0, 0, 25, 0]
@@ -24,26 +26,36 @@ def main():
         user = input("1.Farm Scorpions\n\n>> ")
         if user == '1':
             while True:
-                pb.activateWindow(window_num)
+                pb.activateWindow(HWND, window_num)
                 
-                max_val, max_loc = pb.find(HEALTHBAR, window_num)
-                if max_val >= 0.8:
+                max_val, max_loc = pb.find(HEALTHBAR, HWND, window_num)
+                if max_val >= 0.8:  #Try changing this if youre geting too many false positive or not enough matches. 
+                                    #it goes from 0 to 1. 0: match everything. 1: match only exact matches to the photo
                     print("Fighting... changing window.") 
                     pb.sleep(1,2)
                 else:
-                    max_val, max_loc = pb.find(SCORPION, window_num, scorpion_hsv)
+                    max_val, max_loc = pb.find(SCORPION, HWND, window_num, scorpion_hsv)
                     print(max_val)
                     if max_val >= 0.5:
                         w, h = pb.getCenter(max_loc, SCORPION)
-                        pb.moveMouse(w, h, True, window_num)
+                        e = pb.moveMouse(HWND, w, h, True, window_num)
+                        if e == 1:
+                            e = 0
+                            continue
                         pb.sleep(0.1, 0.15)
                         pb.click()
                         pb.sleep(2.5, 3)
+                    else:
+                        n_ctr += 1
+                        if n_ctr > 5:    
+                            print("Not finding good matches...")
+                            n_ctr = 0
                 if window_num < win_ctr-1:
                     window_num +=1
                 else:
                     window_num = 0
-                print(window_num)
-            
+        if keyboard.is_pressed("q"):
+            print(keyboard.is_pressed("q"))
+            break
                         
 main()
